c4824197332de4dde49b686c76d2ea8d804522bd
JetiTheS <jetro.erakangas@myy.haaga-helia.fi>
2024-08-22 17:07:43 +0300

    Tunnilla tehdyt lisäykset

--- App.js
+++ App.js
@@ -1,10 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, Button, StyleSheet, Text, View, TextInput, Image } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const [text, setText] = useState("");
+
+  const buttonPressed = () => {
+    Alert.alert(text)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Hello world</Text>
+      <Text>Hello maailma</Text>
+      <TextInput
+        placeholder='Enter text here'
+        onChangeText={text => setText(text)}
+        value={text}
+      />
+      <Button onPress={buttonPressed} title="Press me" />
+      <Image
+        style={{ height: 100, width: 100 }}
+        source={require('./assets/icon.png')} />
       <StatusBar style="auto" />
     </View>
   );
@@ -15,6 +31,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-around',
   },
 });
